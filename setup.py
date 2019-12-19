a388bf3daea9c910e41c7d513980226680f7f42a
Carsten Thiele <software@carsten-thiele.de>
2019-12-19 22:45:33 +0100

    Basic Project setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="goecharger",
+    version="0.0.1",
+    author="Carsten Thiele",
+    author_email="software@carsten-thiele.de",
+    description="A Python API for accessing the go-eCharger EV-Charger",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cathiele/goecharger",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
